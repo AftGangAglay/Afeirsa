9871c59d9496beecaac935d51772620aaa09905e
ttg <prs.ttg+gh@pm.me>
2023-08-13 22:00:59 +0100

    misc: fix wrong licence date (oops)

--- examples/main.c
+++ examples/main.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2023 Emily "TTG" Banerjee <prs.ttg+afeirsa@pm.me>
+ */
+
 #include <afeirsa/afeirsa.h>
 
 #include <afeirsa/afgl.h>
