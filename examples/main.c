68147a4310db11ed9ac7fc615290509dd81be529
ttg <prs.ttg+gh@pm.me>
2023-08-20 20:27:28 +0100

    examples: clean up main

--- examples/main.c
+++ examples/main.c
@@ -4,15 +4,8 @@
  */
 
 #include <afeirsa/afeirsa.h>
-
-#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-
-#define WIDTH_F (640.0f)
-#define HEIGHT_F (480.0f)
-
-#define WIDTH_I ((int) WIDTH_F)
-#define HEIGHT_I ((int) HEIGHT_F)
+#include <math.h>
 
 GLFWwindow* mkglfw(void);
 void killglfw(GLFWwindow* window);
@@ -22,12 +15,6 @@ struct vertex {
 	float col[4];
 	float uv[2];
 	float norm[3];
-} __attribute__((packed));
-
-struct transform {
-	float pos[3];
-	float rot[3];
-	float scale[3];
 };
 
 static af_uchar_t pattern(void) {
@@ -81,11 +68,9 @@ int main(void) {
 		}
 	};
 
-	float mouse[2] = { 0 };
-
 	struct af_vert_element vert_elements[] = {
 		{ AF_MEMBSIZE(struct vertex, pos ), AF_VERT_POS  },
-		{ AF_MEMBSIZE(struct vertex, col ), AF_VERT_NONE },
+		{ AF_MEMBSIZE(struct vertex, col ), AF_VERT_COL  },
 		{ AF_MEMBSIZE(struct vertex, uv  ), AF_VERT_UV   },
 		{ AF_MEMBSIZE(struct vertex, norm), AF_VERT_NORM },
 	};
@@ -104,24 +89,18 @@ int main(void) {
 	af_uchar_t texdata[64 * 64 * 4];
 	af_size_t i;
 
+	float clear[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
 	struct af_drawop drawops[] = {
 		{ AF_SETTEX, { 0 } },
 		{ AF_DRAWBUF, { 0 } }
 	};
-
-	struct transform cam = { 0 };
-
-	float clear[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
 	drawops[0].data.settex = &tex;
 
 	drawops[1].data.drawbuf.vert = &vert;
 	drawops[1].data.drawbuf.buf = &buf;
 	drawops[1].data.drawbuf.primitive = AF_TRIANGLE_FAN;
-
-	cam.scale[0] = 1.0f;
-	cam.scale[1] = 1.0f;
-	cam.scale[2] = 1.0f;
 
 	for(i = 0; i < AF_ARRLEN(texdata); ++i) {
 		texdata[i] = pattern();
@@ -140,34 +119,14 @@ int main(void) {
 
 	AF_CHK(af_mkdrawlist(&ctx, &drawlist, drawops, AF_ARRLEN(drawops)));
 
-	AF_CHK(af_setview(&ctx, WIDTH_I, HEIGHT_I));
-
 	while(!glfwWindowShouldClose(window)) {
-		float mouse_delta[2];
-
-		{
-			double x, y;
-			glfwGetCursorPos(window, &x, &y);
-
-			mouse_delta[0] = ((float) x) - mouse[0];
-			mouse_delta[1] = ((float) y) - mouse[1];
-
-			mouse[0] = (float) x;
-			mouse[1] = (float) y;
-		}
-
-		cam.rot[1] += mouse_delta[0];
-		cam.rot[0] += mouse_delta[1];
-
-		{
-			float proj[] = {
-				1.609476f,     0.0f,  0.0f,  0.0f,
-				     0.0f, 2.414213,  0.0f,  0.0f,
-				     0.0f,     0.0f, -1.0f, -1.0f,
-				     0.0f,     0.0f, -0.2f,  0.0f
-			};
-			AF_CHK(af_setparam(&ctx, "u_projection", &projection, proj));
-		}
+		float proj[] = {
+			1.5f, 0.0f,  0.0f,  0.0f,
+			0.0f, 2.5f,  0.0f,  0.0f,
+			0.0f, 0.0f, -1.0f, -1.0f,
+			0.0f, 0.0f, -0.5f,  0.0f
+		};
+		AF_CHK(af_setparam(&ctx, "u_projection", &projection, proj));
 
 		AF_CHK(af_clear(&ctx, clear));
 
@@ -178,7 +137,6 @@ int main(void) {
 				 0.0f, -1.0f,  0.0f, 0.0f,
 				 0.0f, -2.0f,  0.0f, 1.0f
 			};
-
 			AF_CHK(af_setparam(&ctx, "u_model_view", &modelview, mv));
 			AF_CHK(af_draw(&ctx, &drawlist));
 		}
@@ -188,9 +146,10 @@ int main(void) {
 				 1.0f, 0.0f,  0.0f, 0.0f,
 				 0.0f, 1.0f,  0.0f, 0.0f,
 				 0.0f, 0.0f,  1.0f, 0.0f,
-				-2.0f, 0.0f, -4.0f, 1.0f
+				-1.0f, 0.0f, -4.0f, 1.0f
 			};
-
+			float p = (float) sin(glfwGetTime());
+			mv[12] += p;
 			AF_CHK(af_setparam(&ctx, "u_model_view", &modelview, mv));
 			AF_CHK(af_draw(&ctx, &drawlist));
 		}
@@ -215,7 +174,7 @@ GLFWwindow* mkglfw(void) {
 
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	window = glfwCreateWindow(WIDTH_I, HEIGHT_I, "Afeirsa Test", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Afeirsa Test", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
 	return window;
