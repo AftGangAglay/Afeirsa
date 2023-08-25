d74f3b4b2178c71a420706b4a55d45bba574295e
ttg <prs.ttg+gh@pm.me>
2023-08-26 00:46:58 +0100

    examples: improved main error handling

--- examples/main.c
+++ examples/main.c
@@ -6,9 +6,32 @@
 #include <afeirsa/afeirsa.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void echk(const char* proc, enum af_err e) {
+	const char* n;
+	switch(e) {
+		default:
+			AF_FALLTHROUGH;
+			/* FALLTHRU */
+		case AF_ERR_NONE: return;
+
+		case AF_ERR_UNKNOWN: n = "unknown"; break;
+        case AF_ERR_BAD_PARAM: n = "bad parameter"; break;
+        case AF_ERR_BAD_CTX: n = "bad context"; break;
+        case AF_ERR_BAD_OP: n = "bad operation"; break;
+        case AF_ERR_NO_GL: n = "no opengl"; break;
+        case AF_ERR_MEM: n = "out of memory"; break;
+	}
+
+	fprintf(stderr, "%s: %s\n", proc, n);
+	abort();
+}
 
 GLFWwindow* mkglfw(void);
 void killglfw(GLFWwindow* window);
+static void glfwchk(const char* proc);
 
 struct vertex {
 	float pos[3];
@@ -105,18 +128,22 @@ int main(void) {
 		texdata[i] = pattern();
 	}
 
-	AF_CHK(af_mkctx(&ctx, AF_FIDELITY_FAST));
+	echk("af_mkctx", af_mkctx(&ctx, AF_FIDELITY_FAST));
 
-	AF_CHK(af_mkvert(&ctx, &vert, vert_elements, AF_ARRLEN(vert_elements)));
+	echk(
+		"af_mkvert",
+		af_mkvert(&ctx, &vert, vert_elements, AF_ARRLEN(vert_elements)));
 
-	AF_CHK(af_mkbuf(&ctx, &buf, AF_BUF_VERT));
-	AF_CHK(af_upload(&ctx, &buf, vertices, sizeof(vertices)));
+	echk("af_mkbuf", af_mkbuf(&ctx, &buf, AF_BUF_VERT));
+	echk("af_upload", af_upload(&ctx, &buf, vertices, sizeof(vertices)));
 
-	AF_CHK(af_mkbuf(&ctx, &tex, AF_BUF_TEX));
+	echk("af_mkbuf", af_mkbuf(&ctx, &tex, AF_BUF_TEX));
 	tex.tex_width = 64;
-	AF_CHK(af_upload(&ctx, &tex, texdata, sizeof(texdata)));
+	echk("af_upload", af_upload(&ctx, &tex, texdata, sizeof(texdata)));
 
-	AF_CHK(af_mkdrawlist(&ctx, &drawlist, drawops, AF_ARRLEN(drawops)));
+	echk(
+		"af_mkdrawlist",
+		af_mkdrawlist(&ctx, &drawlist, drawops, AF_ARRLEN(drawops)));
 
 	while(!glfwWindowShouldClose(window)) {
 		float proj[] = {
@@ -125,9 +152,11 @@ int main(void) {
 			0.0f, 0.0f, -1.0f, -1.0f,
 			0.0f, 0.0f, -0.5f,  0.0f
 		};
-		AF_CHK(af_setparam(&ctx, "u_projection", &projection, proj));
+		echk(
+			"af_setparam",
+			af_setparam(&ctx, "u_projection", &projection, proj));
 
-		AF_CHK(af_clear(&ctx, clear));
+		echk("af_clear", af_clear(&ctx, clear));
 
 		{
 			float mv[] = {
@@ -136,8 +165,10 @@ int main(void) {
 				 0.0f, -1.0f,  0.0f, 0.0f,
 				 0.0f, -2.0f,  0.0f, 1.0f
 			};
-			AF_CHK(af_setparam(&ctx, "u_model_view", &modelview, mv));
-			AF_CHK(af_draw(&ctx, &drawlist));
+			echk(
+				"af_setparam",
+				af_setparam(&ctx, "u_model_view", &modelview, mv));
+			echk("af_draw", af_draw(&ctx, &drawlist));
 		}
 
 		{
@@ -147,41 +178,67 @@ int main(void) {
 				 0.0f, 0.0f,  1.0f, 0.0f,
 				-1.0f, 0.0f, -4.0f, 1.0f
 			};
-			float p = (float) sin(glfwGetTime());
+			float p;
+
+			double t = glfwGetTime();
+			glfwchk("glfwGetTime");
+
+			p = (float) sin(t);
 			mv[12] += p;
-			AF_CHK(af_setparam(&ctx, "u_model_view", &modelview, mv));
-			AF_CHK(af_draw(&ctx, &drawlist));
+
+			echk(
+				"af_setparam",
+				af_setparam(&ctx, "u_model_view", &modelview, mv));
+			echk("af_draw", af_draw(&ctx, &drawlist));
 		}
 
 		glfwPollEvents();
+		glfwchk("glfwPollEvents");
+
 		glfwSwapBuffers(window);
+		glfwchk("glfwSwapBuffers");
 	}
 
-	AF_CHK(af_killdrawlist(&ctx, &drawlist));
-	AF_CHK(af_killbuf(&ctx, &tex));
-	AF_CHK(af_killvert(&ctx, &vert));
-	AF_CHK(af_killbuf(&ctx, &buf));
-	AF_CHK(af_killctx(&ctx));
+	echk("af_killdrawlist", af_killdrawlist(&ctx, &drawlist));
+	echk("af_killbuf", af_killbuf(&ctx, &tex));
+	echk("af_killvert", af_killvert(&ctx, &vert));
+	echk("af_killbuf", af_killbuf(&ctx, &buf));
+	echk("af_killctx", af_killctx(&ctx));
 
 	killglfw(window);
 
 	return 0;
 }
 
+void glfwchk(const char* proc) {
+	const char* n;
+	if(!glfwGetError(&n)) return;
+	fprintf(stderr, "%s: %s\n", proc, n);
+	abort();
+}
+
 GLFWwindow* mkglfw(void) {
 	GLFWwindow* window;
 
 	glfwInit();
+	glfwchk("glfwInit");
 
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwchk("glfwWindowHint");
 
 	window = glfwCreateWindow(640, 480, "Afeirsa Test", NULL, NULL);
+	glfwchk("glfwCreateWindow");
+
 	glfwMakeContextCurrent(window);
+	glfwchk("glfwMakeContextCurrent");
 
 	return window;
 }
 
 void killglfw(GLFWwindow* window) {
 	glfwDestroyWindow(window);
+	glfwchk("glfwDestroyWindow");
+
 	glfwTerminate();
+	glfwchk("glfwTerminate");
 }
